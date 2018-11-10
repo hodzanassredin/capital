56e574285439aee7dedf234895624dbc2b705e49
Shelin Anton <shelinanton@gmail.com>
2018-11-10 20:02:36 +0300

    init

---
+++ src/Domain/Library.fs
@@ -0,0 +1,12 @@
+namespace Domain
+open System.Collections.Generic
+
+module Capital =
+    type Category = string
+    type Quantity = float
+
+    type Environment = Dictionary<Category, Quantity> 
+
+    
+
+    
